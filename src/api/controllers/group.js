16fbe594eae136def27cb26cc168772b9eb791f9
ngatnx <nxn1339@gmail.com>
2024-04-09 23:46:07 +0700

    add create group api

--- src/api/controllers/group.js
+++ src/api/controllers/group.js
@@ -17,6 +17,45 @@ async function getListGroup(id_user) {
     }
 }
 
+async function getListMember(id_group) {
+    try {
+        const result = await db.execute(
+            `SELECT \`id\`, \`name\`, \`avatar\`
+            FROM user 
+            INNER JOIN member 
+            WHERE 
+            user.id = member.id_user 
+            and member.id_group = '${id_group}';`)
+        return {
+            code: 200,
+            data: result
+        }
+    } catch (error) {
+        throw (error)
+    }
+}
+
+async function createGroup(group) {
+    try {
+        await db.execute(
+            `INSERT INTO \`group\`(\`id\`, \`name\`, \`image\`, \`id_user\`) 
+            VALUES (
+                uuid(),
+                '${group.name}',
+                '${group.image??""}',
+                '${group.id_user}')`
+        )
+        return {
+            code: 200,
+            data: 'Tạo nhóm thành công !'
+        }
+    } catch (error) {
+        throw (error)
+    }
+}
+
 module.exports = {
-    getListGroup
+    getListGroup,
+    getListMember,
+    createGroup
 }
--- src/api/routers/group.js
+++ src/api/routers/group.js
@@ -10,4 +10,20 @@ router.get('/:id_user', async (req, res, next) => {
     }
 })
 
+router.get('/member/:id_group', async (req, res, next) => {
+    try {
+        res.json(await controller.getListMember(req.params.id_group))
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    try {
+        res.json(await controller.createGroup(req.body))
+    } catch (error) {
+        next(error)
+    }
+})
+
 module.exports = router
